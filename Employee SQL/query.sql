4a808b76c59728a1acb98b69f9bc9de6c0682cc2
ese12880 <70610771+ese12880@users.noreply.github.com>
2021-01-13 12:20:22 -0600

    commit

--- Employee SQL/query.sql
+++ Employee SQL/query.sql
@@ -1,28 +1,49 @@
-SELECT employees.emp_no, dept_employees.dept_no, departments.dept_name, employees.emp_title, employees.birth_date, employees.first_name, employees.last_name, employees.sex, employees.hire_date, salaries.salary 
-FROM employees
-INNER JOIN salaries on employees.emp_no = salaries.emp_no
-INNER JOIN dept_employees ON employees.emp_no = dept_employees.emp_no
-inner join departments on dept_employees.dept_no = departments.dept_no
-inner join dept_managers on departments.dept_no = dept_managers.dept_no
-ORDER BY employees.emp_no ASC;
-
+-- 1
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.Salary 
 FROM employees 
 LEFT JOIN salaries ON employees.emp_no = salaries.emp_no
 
+-- 2
 SELECT employees.last_name, employees.first_name, employees.hire_date
 FROM employees 
-where hire_date >= 01/01/1986 and hire_date <= 12/31/1986
+WHERE hire_date BETWEEN '01/01/1986 12:00am' and '12/31/1986 11:59pm'
 
+-- 3
 SELECT dept_managers.dept_no, departments.dept_name, dept_managers.emp_no, employees.last_name, employees.first_name
-FROM employees 
-LEFT JOIN dept_managers ON employees.emp_no = dept_managers.dept_no
+FROM employees
+left JOIN dept_managers ON employees.emp_no = dept_managers.dept_no
 right join departments on employees.emp_no = departments.dept_no
 
+-- 4
 select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 from employees
 left join departments on employees.emp_no = departments.dept_no
 
+-- 5
 select first_name, last_name, sex
 from employees
-where first_name = 'hercules'
+where first_name = 'Hercules' 
+
+-- 6
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees 
+group by dept_name = Sales
+left join departments on employees.emp_no = departments.dept_no
+
+-- 7
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+where dept_name = sales and dept_name = development
+left join departments on employees.emp_no = departments.dept_name
+
+
+-- 8
+SELECT last_name, COUNT(last_name) as "employee count"
+FROM employees
+GROUP BY last_name
+ORDER BY "employee count" DESC;
+
+
+
+
+
